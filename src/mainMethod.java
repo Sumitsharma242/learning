ba25caf55803b9ca4852162c4968fb58c54246bd
Sumitsharma242 <88539280+Sumitsharma242@users.noreply.github.com>
2025-09-30 16:04:20 +0200

    Main method overloading

---
+++ src/mainMethod.java
@@ -0,0 +1,16 @@
+public class mainMethod {
+
+    public static void main(String[] args) {
+        System.out.println("main method is running");
+        main(new int[]{1,2,3});
+    }
+    // main method overloading but by default JVM will call the main method with String[] args
+    public static void main(int[]... args){
+        for (int[] x:args) {
+            for (int y:x) {
+                 System.out.println(y);
+             }
+        }
+        System.out.println("submain function is running");
+    }
+}
