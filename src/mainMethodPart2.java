93a3585284e7d5beea374a45849297cd97f3bc35
Sumitsharma242 <88539280+Sumitsharma242@users.noreply.github.com>
2025-09-30 16:25:26 +0200

    Static method before the main method

---
+++ src/mainMethodPart2.java
@@ -0,0 +1,12 @@
+public class mainMethodPart2 {
+
+    // this came in the 1.7 version of java where the main is found and then identify the static block,
+    //then while execute the static block, then the main method
+    // but while the output is first the static block and then the main method
+    static{
+        System.out.println("This is the static block");
+    }
+    public static void main(String[] args){
+        System.out.println("This is the main method");
+    }
+}
