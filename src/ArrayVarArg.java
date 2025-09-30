f57ed2b43553a700bb537fd9319b383db268e771
Sumitsharma242 <88539280+Sumitsharma242@users.noreply.github.com>
2025-09-30 10:41:04 +0200

    replaced variable with Var arg Array

--- src/ArrayVarArg.java
+++ src/ArrayVarArg.java
@@ -1,7 +1,7 @@
 public class ArrayVarArg {
     public static void main(String[] args) {
-        int []array={10,20,30};
-        sum(1, array);
+    //    int []array={10,20,30};
+        sum(1, new int []{10,20,30});
     }
     public static void sum(int y,int... x){
         int sum=0;
