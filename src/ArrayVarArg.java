ba651c7bdb3f253a33ed421949a36699ba3da678
Sumitsharma242 <88539280+Sumitsharma242@users.noreply.github.com>
2025-09-30 11:01:29 +0200

    having varArg with multiple arrays

--- src/ArrayVarArg.java
+++ src/ArrayVarArg.java
@@ -1,7 +1,7 @@
 public class ArrayVarArg {
     public static void main(String[] args) {
-    //    int []array={10,20,30};
-        sum(1, new int []{10,20,30});
+        //    int []array={10,20,30};
+/*        sum(1, new int []{10,20,30});
     }
     public static void sum(int y,int... x){
         int sum=0;
@@ -10,5 +10,14 @@ public class ArrayVarArg {
         }
         int z= sum-y;
         System.out.println("Sum of numbers is: "+z);
+  */
+        int[] a1 = {10, 20, 30};
+        int[] a2 = {1, 2, 3};
+        int[] a3 = {4, 5, 6};
+        sum(a1, a2, a3);
+    }
+    public static void sum(int[]... x) {
+        for (int []y:x)
+            System.out.println(y[0]);
     }
 }
