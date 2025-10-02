f51d1e8d07266bfdd6a664e28365d8159edc463d
Sumitsharma242 <88539280+Sumitsharma242@users.noreply.github.com>
2025-10-02 13:20:47 +0200

    this is the program for input from command line

---
+++ src/cmdLineArguments.java
@@ -0,0 +1,15 @@
+public class cmdLineArguments {
+    // in this input is taker from the command line based on what is the input actions can be take out
+//    public static void main(String[] args){
+//        int n= Integer.parseInt(args[0]);
+//                System.out.println("The square of "+n+" is "+n*n);
+//    }
+
+//------------------------------------------
+    // this is to print the command line arguments without any action
+    public static void main(String[] args){
+        for (int i=0;i<args.length;i++){
+            System.out.println("The argument at "+i+" is "+args[i]);
+        }
+    }
+}
