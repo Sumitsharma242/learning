16f8b50b6eeaa470b33f772b775ddd09f93f0c21
Sumitsharma242 <88539280+Sumitsharma242@users.noreply.github.com>
2025-10-27 15:37:57 +0100

    For each loop

---
+++ src/learningJava/For_each_loop.java
@@ -0,0 +1,23 @@
+package learningJava;
+
+public class For_each_loop {
+    public static void main(String[] args) {
+        int[][] numbers = {{2, 3, 4, 5},{10,20,30,40}};
+        // with this we can print 2D array using for each loop
+
+        for (int[] number : numbers) {
+            for (int test:number) {
+                System.out.println(test);
+            }
+        }
+        // with this we can print 3D array using for each loop
+          int [][][]x = {{{1,2,3},{4,5,6}},{{7,8,9},{10,11,12}},{{13,14,15},{16,17,18}}};
+        for (int[][] y:x) {
+            for (int[] z:y) {
+                for (int a:z) {
+                    System.out.println("This is the output form 3d array"+a);
+                }
+            }
+        }
+    }
+}
