50125387cd6a3590e06c895c8180fd65f3043c2b
Sumitsharma242 <88539280+Sumitsharma242@users.noreply.github.com>
2025-12-11 10:31:54 +0100

    practise for loops

---
+++ src/learningJava/PractiseSet_6_Array.java
@@ -0,0 +1,47 @@
+package learningJava;
+
+public class PractiseSet_6_Array {
+    public static void main(String[] args) {
+//        int [] array={10,20,30,40,50};
+//        for (int i=0;i< array.length;i++){
+//         if (array[i]==30){System.out.println("The index of 30 is: "+i);
+//             break;}
+//
+//         }
+
+//int sum=0;
+//        int [] marks={20,30,40,50};
+//        for(int k: marks){
+//
+//                sum= sum+k;
+//        System.out.print(" The average of marks is: "+sum/marks.length);
+//            }
+//     System.out.println(" ");
+
+//
+
+
+//        int [] arr={10,20,30,40,50};
+//
+//        int x= Math.floorDiv(arr.length,2);
+//        for (int i=0;i<x;i++){
+//            int temp= arr[i];
+//            arr[i]= arr[arr.length-1-i];
+//            arr[arr.length-1-i]= temp;
+//
+//        }
+//        for (int k:arr){
+//            System.out.print(k+" ");
+//        }
+
+
+//        int [] arr={30,10,20,40,50};
+//        int element=Integer.MAX_VALUE;
+//        for (int i=0;i<arr.length;i++){
+//            if(arr[i]<element){
+//                element=arr[i];
+//            }
+//        }
+//        System.out.println(element);
+     }
+    }
